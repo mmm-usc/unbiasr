47d2e532aa8caf4bb4ff49b833306f42b717a042
Clay Cantrell <cacantre@usc.edu>
2021-06-22 12:11:31 -0700

    fixed validations for focal groups

--- pageOneServer.R
+++ pageOneServer.R
@@ -226,6 +226,19 @@ output$distPlot <- renderPlot({
       length(lambda_rNumeric()) == length(tau_rNumeric()),
       "Factor loadings and intercepts need to have the same value\n"
     ),
+    if(input$uselambda_f == TRUE){
+      need(
+        length(lambda_rNumeric()) == length(lambda_fNumeric()),
+        "factor loadings for referance and focal groups need to have the same number of values\n"
+      )
+    },
+    if(input$usetau_f == TRUE){
+      need(
+        length(tau_rNumeric()) == length(tau_fNumeric()),
+        "intercepts for referance and focal groups need to have the same number of values\n"
+      )
+    },
+    
     #only checks for numeric input of theta_r when matrix is not being used as input
     if (input$useMatrix == FALSE) {
       need(input$theta_r,
@@ -296,6 +309,18 @@ output$table <- renderTable(rownames = TRUE, {
       length(lambda_rNumeric()) == length(tau_rNumeric()),
       "loadings and intercepts need to have the same dimension\n"
     ),
+    if(input$uselambda_f == TRUE){
+      need(
+        length(lambda_rNumeric()) == length(lambda_fNumeric()),
+        "factor loadings for referance and focal groups need to have the same number of values\n"
+      )
+    },
+    if(input$usetau_f == TRUE){
+      need(
+        length(tau_rNumeric()) == length(tau_fNumeric()),
+        "intercepts for referance and focal groups need to have the same number of values\n"
+      )
+    },
     #only checks for numeric input of theta_r when matrix is not being used as input
     if (input$useMatrix == FALSE) {
       need(input$theta_r,
