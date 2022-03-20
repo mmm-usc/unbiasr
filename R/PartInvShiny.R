1ca25fa027ad1bc90f315b7fc8c347407f6396c4
Yichizhang2024 <yzhang97@usc.edu>
2022-03-20 16:10:18 -0700

    add a new page for MCAA

--- R/PartInvShiny.R
+++ R/PartInvShiny.R
@@ -10,7 +10,7 @@ myApp <- function(...) {
   ui <- dashboardPage(
     dashboardHeader(title = "Computation of Selection Accuracy Indexes",
                     titleWidth = 400),
-    dashboardSidebar(width = 105,
+    dashboardSidebar(width = 150,
                      sidebarMenu(
                        menuItem(
                          "Instruction",
@@ -18,12 +18,13 @@ myApp <- function(...) {
                          icon = icon("dashboard")
                        ),
                        menuItem(
-                         "FirstPage",
-                         tabName = "firstpage",
+                         "Single dimension",
+                         tabName = "Single",
                          icon = icon("dashboard")
                        ),
                        #unused
-                       menuItem("Other", tabName = "other", icon = icon("table"))
+                       menuItem("Multidimensions", tabName = "Multiple", 
+                                icon = icon("dashboard"))
                      )),
     dashboardBody(
       #include for shinyjs features to work
@@ -84,8 +85,8 @@ myApp <- function(...) {
                     img(src="step5.png", align = "center",height='300px',width='270px'),
                     p("Here are the graph and the selection accuracy table"
                     ),
-                    img(src="graph.png", align = "left",height='300px',width='330px'),
-                    img(src="table.png", align = "right",height='280px',width='350px'),
+                    img(src="graph.png", align = "left",height='300px',width='300px'),
+                    img(src="table.png", align = "right",height='280px',width='330px'),
                   ),
                   
                   box(
@@ -117,7 +118,7 @@ myApp <- function(...) {
                   )
                 ))),
         #second page
-        tabItem(tabName = "firstpage", fluidPage(
+        tabItem(tabName = "Single", fluidPage(
           #wrap all UI code in list command so it can be accessed from main app.R file
           list(
             #withMathJax adds HTML for greek characters
@@ -292,7 +293,7 @@ myApp <- function(...) {
                        status = "primary",
                        solidHeader = FALSE,
                        width = 12,
-                       materialSwitch("usepropsel", "Select 10% population?", status = "primary", FALSE),
+                       materialSwitch("usepropsel", "Selection based on percentage?", status = "primary", FALSE),
                        #numeric input for single number values
                        numericInput(
                          "cut_z",
@@ -344,7 +345,240 @@ myApp <- function(...) {
           
         )),
         #second page
-        tabItem(tabName = "other", fluidPage(fluidRow()))
+        tabItem(tabName = "Multiple", fluidPage(
+          #wrap all UI code in list command so it can be accessed from main app.R file
+          list(
+            #withMathJax adds HTML for greek characters
+            withMathJax(),
+            #first row which contains the directions
+            fluidRow(
+              #no column needed for this row because there is only one box in the row
+              box(
+                title = "Directions",
+                #status controls the color
+                status = "primary",
+                #using a 12-column responsive grid, so this box (width 12) takes up the full container its in, in this case the full row.
+                width = 12,
+                solidHeader = FALSE,
+                p(
+                  "This application is designed for the use of the Multidimensional Classification Accuracy Analysis (MCAA) Framework
+                      for evaluating measurement invariance in the personnel selection context. By entering the parameter estimates from the factor model,
+                      such as the factor loadings and intercepts, you will be able to visualize the impact of item bias on selection accuracy indices and
+                      get a table that summarize the change of selection indices."
+                )
+              )
+            ),
+            #second row which contains input and output columns
+            fluidRow(
+              #input column (width 4/12 of the page)
+              column(width = 4,
+                     box(
+                       title = "Inputs",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       #all boxes in this column take up the full width of the column (12/12)
+                       width = 12,
+                       #HTML styling to increase bottom padding of reset button
+                       div(style = "padding-bottom: 10px;",
+                           #reset button
+                           actionButton("resetButton", "reset inputs")),
+                       #text inputs for graph labels
+                       textInput('legend_r',
+                                 'Input reference group label',
+                                 #default value
+                                 value = "Reference group"),
+                       textInput('legend_f',
+                                 'Input focal group label',
+                                 value = "Focal group")
+                     ),
+                     box(
+                       title = "Intercepts and Loadings",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       width = 12,
+                       #text inputs for multi-value inputs
+                       textInput(
+                         'lambda_r',
+                         'Reference group factor loadings \\( \\lambda \\)',
+                         placeholder = "1.00, 1.66, 2.30, 2.29"
+                       ),
+                       textInput(
+                         'lambda_f',
+                         'Input factor loadings \\( \\lambda \\) for the focal group',
+                         placeholder = "1.00, 1.66, 2.30, 2.29"
+                       ),
+                       #materialSwitch button using shinyWidgets
+                       materialSwitch("uselambda_f", "Focal group?", status = "primary", FALSE),
+                       textInput(
+                         'tau_r',
+                         'Reference group measurement intercepts \\( \\tau \\)',
+                         placeholder = "1.54, 1.36, 1.16, 1.08"
+                       ),
+                       textInput(
+                         'tau_f',
+                         'Input measurement intercepts \\( \\tau \\) for the focal group',
+                         placeholder = "0.68, 1.36, 1.16, 1.08"
+                       ),
+                       materialSwitch("usetau_f", "Focal group?", status = "primary", FALSE),
+                     ),
+                     box(
+                       title = "Unique Factor Variance-Covariance",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       width = 12,
+                       #HTML style buttons side by side
+                       div(style = "display:inline-block, float:right",
+                           materialSwitch(
+                             "useMatrix",
+                             "Matrix input?",
+                             status = "primary",
+                             FALSE,
+                             inline = TRUE
+                           ),
+                           materialSwitch(
+                             "usetheta_f",
+                             "Focal group?",
+                             status = "primary",
+                             FALSE,
+                             inline = TRUE
+                           ),
+                           sliderInput(
+                             "matrixSlider",
+                             "Matrix Size",
+                             min = 2,
+                             max = 10,
+                             value = 4
+                           )
+                       ),
+                       textInput(
+                         'theta_r',
+                         'Reference group uniqueness \\( \\theta \\)',
+                         placeholder = "1.20, 0.81, 0.32, 0.32"
+                       ),
+                       textInput(
+                         'theta_f',
+                         'Input the diagonal of the unique factor variance-covariance matrix \\( \\Theta \\) for the focal group',
+                         placeholder = "0.72, 0.81, 0.32, 0.32"
+                       ),
+                       #strong is a text output in bold
+                       strong(
+                         id = "theta_rMatrixTitle",
+                         "input the unique factor variance-covariance matrix \\( \\Theta \\) for the referance group"
+                       ),
+                       #uiOutput used for dynamic inputs, in this case matrices with variable size
+                       #logic defined in renderUI in pageOneServer.R
+                       uiOutput("theta_rMatrixUI"),
+                       strong(
+                         id = "theta_fMatrixTitle",
+                         "input the unique factor variance-covariance matrix \\( \\Theta \\) for the focal group"
+                       ),
+                       uiOutput("theta_fMatrixUI"),
+                       
+                     ),
+                     box(
+                       title = "Mean and Variances",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       width = 12,
+                       numericInput(
+                         "kappa_r",
+                         "Latent factor mean \\( \\kappa \\) for the reference group:",
+                         value = 0.5,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       numericInput(
+                         "kappa_f",
+                         "Latent factor mean \\( \\kappa \\) for the focal group:",
+                         value = 0.0,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       materialSwitch("usekappa_f", "Focal group?", status = "primary", FALSE),
+                       numericInput(
+                         "phi_r",
+                         "Latent factor variance \\( \\phi \\) for the reference group:",
+                         value = 1.,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       numericInput(
+                         "phi_f",
+                         "Latent factor variance \\( \\phi \\) for the focal group:",
+                         value = 1.,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       materialSwitch("usephi_f", "Focal group?", status = "primary", FALSE),
+                     ),
+                     box(
+                       title = "Cutoffs and Mixing Proportion",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       width = 12,
+                       materialSwitch("usepropsel", "Selection based on percentages?", status = "primary", FALSE),
+                       #numeric input for single number values
+                       numericInput(
+                         "cut_z",
+                         "Cutoff score on the observed composite:",
+                         value = 0.5,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       numericInput(
+                         "prop",
+                         "Selection proportion:",
+                         value = 0.5,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                       numericInput(
+                         "pmix",
+                         "Mixing proportion:",
+                         value = 0.5,
+                         min = 0,
+                         max = 1,
+                         step = 0.01
+                       ),
+                     ),
+                     box(
+                       title = "Output",
+                       status = "primary",
+                       solidHeader = FALSE,
+                       width = 12,
+                       #show both tables
+                       materialSwitch("tab_strict", "show results for strict invariance", status = "primary", FALSE),
+                       #numeric input for single number values
+              ),
+              box(
+                id = "outputBox",
+                title = "Relationship Between True Latent Construct Scores
+               and Observed Test Scores",
+                status = "primary",
+                solidHeader = FALSE,
+                width = 8,
+                plotOutput("distPlot")
+              ),
+              box(
+                id = "outputTableBox",
+                title = "Impact of Item Bias on Selection Accuracy Indices",
+                status = "primary",
+                solidHeader = FALSE,
+                width = 8,
+                tableOutput("table")
+              )
+            )
+            # ,
+            # downloadButton("downloadData", "Download")
+          )
+          
+        ))
       )
     )
   )
@@ -450,6 +684,7 @@ myApp <- function(...) {
       #reset all
       reset("matrixSlider")
       reset("usepropsel")
+      reset("tab_strict")
       reset("uselambda_f")
       reset("usetau_f")
       reset("usetheta_f")
@@ -644,6 +879,51 @@ myApp <- function(...) {
           tau_r = tau_rNumeric(),
           Theta_r = theta_r(),
           labels = c(input$legend_r, input$legend_f)
+        )[[4]]
+      }
+      else{
+        PartInv(
+          #propsel adds value as input if true
+          propsel = input$prop,
+          plot_contour = TRUE,
+          cut_z = input$cut_z,
+          pmix_ref = input$pmix,
+          kappa_r = input$kappa_r,
+          kappa_f = kappa_f(),
+          phi_r = input$phi_r,
+          phi_f = phi_f(),
+          lambda_r = lambda_rNumeric(),
+          lambda_f = lambda_f(),
+          tau_f = tau_f(),
+          Theta_f = theta_f(),
+          tau_r = tau_rNumeric(),
+          Theta_r = theta_r(),
+          labels = c(input$legend_r, input$legend_f)
+        )[[4]]
+      }
+    })
+    
+    partInvOutput <- reactive({
+      if (input$tab_strict == FALSE) {
+        #plug everything into PartInv function
+        #calls to reactive functions have () brackets
+        PartInv(
+          propsel = input$prop,
+          plot_contour = TRUE,
+          cut_z = input$cut_z,
+          pmix_ref = input$pmix,
+          kappa_r = input$kappa_r,
+          kappa_f = kappa_f(),
+          phi_r = input$phi_r,
+          phi_f = phi_f(),
+          lambda_r = lambda_rNumeric(),
+          lambda_f = lambda_f(),
+          tau_f = tau_f(),
+          Theta_f = theta_f(),
+          tau_r = tau_rNumeric(),
+          Theta_r = theta_r(),
+          labels = c(input$legend_r, input$legend_f),
+          
         )[[4]]
       }
       else{
