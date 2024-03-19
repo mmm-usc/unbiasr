2ce40dec47f982ccd8da857bb993afa05cbe90c1
Meltem Ozcan <mozcan@ucsc.edu>
2024-03-19 10:41:17 -0700

    example

---
+++ R/roc.R
@@ -0,0 +1,123 @@
+auc <- function(SE, SP){
+  TPR <- SE
+  FPR <- 1 - SP
+  dFPR <- c(diff(FPR), 0)
+  dTPR <- c(diff(TPR), 0)
+  sum(TPR * dFPR) + sum(dTPR * dFPR)/2
+}
+
+#SE = TPR, FPR = 1 - SP
+roc_PartInv <- function(cfa_fit,
+                               pmix, 
+                               from = 0.01,
+                               to = 0.25,
+                               by = 0.01,
+                               cutoffs_from = NULL,
+                               cutoffs_to = NULL,
+                               mod_names = c("par", "str")) {
+  
+  est <- format_cfa_partinv(cfa_fit, comp = "est")
+  
+  propsels <- seq(from = from, to = to, by = by)
+  use <- "propsels"
+  xl <- "Proportion of selection"
+  rangeVals <- propsels
+  
+  if ((is.null(cutoffs_from) && !is.null(cutoffs_to)) ||
+      (!is.null(cutoffs_from) && is.null(cutoffs_to))) {
+  #  warning("If you would like to plot CAI at different thresholds, provide
+  #          parameter values for both `cutoffs_to` and `cutoffs_from`.
+   #         CAI were plotted at different proportions of selection by default.")
+  }
+  
+  # if the user provided the max and min cutoff values, update rangeVals with 
+  # a range of cutoffs
+  if (!is.null(cutoffs_from) && !is.null(cutoffs_to)) {
+    cutoffs <- seq(from = cutoffs_from, to = cutoffs_to, by = by)
+    rangeVals <- cutoffs
+    xl <- "Thresholds" # for the plots later
+    use <- "cutoffs"
+  }
+  n_g <- cfa_fit@Data@ngroups # number of groups
+  
+  # if the user did not provide labels, or provided the wrong number of labels,
+  if (is.null(labels) || (length(labels) != n_g)) {
+    labels <- cfa_fit@Data@group.label
+    labels <- paste(labels, c("(reference)", rep("(focal)", n_g - 1)))
+  }
+  cai <- c("Sensitivity", "Specificity")
+  ls_mat <- matrix(NA, ncol = length(rangeVals), nrow = n_g,
+                   dimnames = list(labels, rangeVals))
+  ls_names <- c(t(outer(cai, Y = mod_names, FUN = paste, sep = "_")))
+  ls <- rep(list(ls_mat), length(ls_names))
+  names(ls) <- ls_names
+  
+  # if pmix is missing, assume equal mixing proportions
+  if (is.null(pmix)) pmix <- as.matrix(c(rep(1 / n_g, n_g)), ncol = n_g)
+  pmix <- as.vector(pmix)
+  
+  ylabs <- ""
+  mains <- ""
+  
+  # call PartInv with each proportion of selection and store CAI in the list of
+  # data frames
+  for (p in seq_along(rangeVals)) {
+    # if the user provided cutoff values
+    if (use == "cutoffs") {
+      suppressWarnings({
+        pinv <- PartInv(cut_z = cutoffs[p],
+                        psi = est$psi,
+                        lambda = est$lambda,
+                        theta = est$theta,
+                        alpha = est$alpha,
+                        nu = est$nu,
+                        pmix = pmix,
+                        plot_contour = FALSE,
+                        labels = labels,
+                        show_mi_result = TRUE)
+      })
+    }
+    # if the user did not provide cutoff values
+    if (use == "propsels") {
+      suppressWarnings({
+        pinv <- PartInv(propsel = propsels[p],
+                        psi = est$psi,
+                        lambda = est$lambda,
+                        theta = est$theta,
+                        alpha = est$alpha,
+                        nu = est$nu,
+                        pmix = pmix,
+                        plot_contour = FALSE,
+                        labels = labels,
+                        show_mi_result = TRUE)
+      })
+    }  
+   
+    num_comb <- length(cai) * length(mod_names) + 1 # for specifying the
+    # index within ls
+    ind <- 1
+    
+    while (ind < num_comb) {
+    for(i in cai) {
+        # for each specified invariance condition
+        for (j in seq_along(mod_names)) {
+          # if the specified invariance condition is partial inv.,
+          ls[[ind]][, p] <-
+            ifelse(rep(mod_names[j] == "par", n_g),
+                  pinv$summary[cai,1:n_g][[1]],
+                  pinv$summary_mi[cai,1:n_g][[1]])
+            
+        #  ylabs <- c(ylabs, paste0(cai, " (", cai_names[i], ")"))
+          
+          temp <- ""
+          if (mod_names[j] == "par") temp <- "partial invariance"
+          if (mod_names[j] == "str") temp <- "strict invariance"
+          
+          mains <- c(mains, paste0(cai, " under ", temp))
+          ind <- ind + 1
+        }
+    }
+  }
+  }
+  return(ls)
+}                         
